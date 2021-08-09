8529e8898cfecc40df619cdece2a3670dc70d9ca
Alexander <allemeshov@gmail.com>
2021-08-09 21:24:38 +0300

    group info external component

---
+++ src/app/modules/groups/elements/single-group-info/single-group-info.component.ts
@@ -0,0 +1,71 @@
+import {Component, OnInit} from '@angular/core';
+import {GroupInfoService} from "../../services/group-info.service";
+import {GroupService} from "../../services/group.service";
+import {OutcomeOperationInterface} from "../../interfaces/outcome-operation.interface";
+import {IncomeOperationInterface} from "../../interfaces/income-operation.interface";
+import GroupDto from "../../../../shared/interfaces/group.interface";
+
+@Component({
+  selector: 'single-group-info',
+  templateUrl: './single-group-info.component.html',
+  styleUrls: ['./single-group-info.component.sass']
+})
+export class SingleGroupInfoComponent implements OnInit {
+
+  //#region Fields
+
+  date = '';
+  title = '';
+  totalOutCome = 0;
+  totalIncome = 0;
+  outcomeOperations: OutcomeOperationInterface[] = [];
+  incomeOperations: IncomeOperationInterface[] = [];
+  totalAmount = 0;
+
+  //#endregion
+
+  constructor(
+    private _groupInfoService: GroupInfoService,
+    private _groupService: GroupService,
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.loadDisplayItems();
+  }
+
+  private loadDisplayItems(): void {
+    this.date = this._groupInfoService.date;
+
+    this._groupService.getById(this._groupService.openedGroupId)
+      .subscribe((group: GroupDto) => {
+        this.title = group.title;
+      });
+
+    this.totalOutCome = this._groupInfoService.totalOutcome;
+
+    this.totalIncome = this._groupInfoService.totalIncome;
+
+    this.outcomeOperations = this._groupInfoService.outcomeOperations;
+
+    this.incomeOperations = this._groupInfoService.incomeOperations;
+
+    this.totalAmount = this._groupInfoService.totalAmount;
+  }
+
+  membersClicked(): void {
+  }
+
+  getMembersString(): string {
+    return ''
+  }
+
+  openCreateOperationPage(): void {
+  }
+
+  showInfoCategoryOutcome(operation: OutcomeOperationInterface): void {
+  }
+
+  showInfoCategoryIncome(operation: IncomeOperationInterface): void {
+  }
+}
